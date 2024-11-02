fc38d675d566cc01a865079bc28c6127969ec9ad
Muhammad Hammad Aslam <hammadaslam308@gmail.com>
2024-11-02 21:53:59 +0500

    updated the about section

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -11,8 +11,8 @@ const About = () => {
       <h1 className="pageTitle">About me</h1>
       <div style={AboutCSS.aboutPara}>
         <p>
-          I'm <span className="highlight">Hammad Aslam</span>, an Information
-          Technology student from Jauharabad, Pakistan.
+          I'm <span className="highlight">Hammad Aslam</span>, a recent IT
+          Graduate from Punjab, Pakistan.
         </p>
         <p>
           I have done my Matriculation from Govt. Jauhar Memorial High School,
@@ -37,26 +37,48 @@ const About = () => {
           <span className="highlight">78%</span> marks.
         </p>
         <p>
-          Now I'm a student of BS Information Technology Post ADP 4th sem (8th
-          sem BS(hons)) University of Education, Lahore for the session
-          2022-2024. My CGPA is <span className="highlight">3.61</span>.
+          I have done my BS Information Technology (Post ADP) University of
+          Education, Lahore in the session 2022-2024. My CGPA is{" "}
+          <span className="highlight">3.63</span>.
         </p>
         <p>
-          Currently I am working on my final year project,{" "}
+          I have worked on my final year project,{" "}
           <span className="highlight">
             'Customer Reviews Classification and Analysis System using Data
             Mining and NLP'
           </span>
-          . It is basically a system that analyzes the customer reviews and
-          classify them on the basis of fakeness, sentiments, topics and context
-          extraction. These reviews are taken from various E-commerce platforms
-          like Amazon and Daraz.
+          . Basically it is a system that analyzes the customer reviews and
+          classify them on the basis of topics, sentiments and helpfulness.
+          These reviews are taken from various E-commerce platforms like Amazon
+          and Daraz.
         </p>
         <p>
-          I have interests in programming and web development. I have developed
+          I recently attended an{" "}
+          <span className="highlight">
+            Apprenticeship Program for IT Graduates
+          </span>{" "}
+          under{" "}
+          <span className="highlight">
+            PSEB (Pakistan Software Export Board)
+          </span>{" "}
+          in the known Healthcare IT Company{" "}
+          <span className="highlight">Maaz Informatics</span> in Sargodha, where
+          I spent 2 months as a <span className="highlight">MERN Stack</span>{" "}
+          and <span className="highlight">Next JS</span> apprentice.
+        </p>
+        <p>
+          I worked on a{" "}
+          <span className="highlight">Smart FYP Management System</span>. This
+          system is all about digitalizing the process of Capstone Project
+          between supervisors, coordinators and students of final year.
+        </p>
+        <p>
+          I have interests in Data Science and Web Development. I have developed
           some projects for Web and android in{" "}
           <span className="highlight">React JS</span> and{" "}
-          <span className="highlight">React Native</span> respectively.
+          <span className="highlight">React Native</span> respectively and my
+          Final Year Project was analysis and research based, in which I worked
+          with <span className="highlight">Python</span>
         </p>
         <p>I'm looking for the opportunities to enhance my skills.</p>
         <p>
